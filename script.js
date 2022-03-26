e77a1c80eac92fa4cb83d101b16bc5e920a91db7
Guilherme Kvet <guilhermevkvet22@gmail.com>
2022-03-26 20:24:24 -0300

    Adicionando arquivos projeto Meme Generator

--- script.js
+++ script.js
@@ -31,3 +31,83 @@ function uploadImg() {
     })
 }
 uploadImg();
+
+function buttonFire(){
+    let button = document.getElementById('fire');
+    let container = document.getElementById('meme-image-container');
+
+    button.addEventListener('click', function(){
+        container.className = 'fire';
+    })
+}
+buttonFire();
+
+function buttonWater(){
+    let button = document.getElementById('water');
+    let container = document.getElementById('meme-image-container');
+
+    button.addEventListener('click', function(){
+        container.className = 'water';
+    })
+}
+buttonWater();
+
+function buttonEarth(){
+    let button = document.getElementById('earth');
+    let container = document.getElementById('meme-image-container');
+
+    button.addEventListener('click', function(){
+        container.className = 'earth';
+    })
+}
+buttonEarth();
+
+function buttonBlack(){
+    let button = document.getElementById('black');
+    let container = document.getElementById('meme-image-container');
+
+    button.addEventListener('click', function(){
+        container.className = 'black';
+    })
+}
+buttonBlack();
+
+function meme1 (){
+    let meme = document.getElementById('meme-1');
+    let image = document.getElementById('meme-image');
+
+    meme.addEventListener('click', function(){
+        image.src = meme.src
+    })
+}
+meme1();
+
+function meme2 (){
+    let meme = document.getElementById('meme-2');
+    let image = document.getElementById('meme-image');
+
+    meme.addEventListener('click', function(){
+        image.src = meme.src
+    })
+}
+meme2();
+
+function meme3 (){
+    let meme = document.getElementById('meme-3');
+    let image = document.getElementById('meme-image');
+
+    meme.addEventListener('click', function(){
+        image.src = meme.src
+    })
+}
+meme3();
+
+function meme4 (){
+    let meme = document.getElementById('meme-4');
+    let image = document.getElementById('meme-image');
+
+    meme.addEventListener('click', function(){
+        image.src = meme.src
+    })
+}
+meme4();
